bb8add6719479460d0b6071c06b35983731b4dd8
Olubiyo Aliyah <aliyaholubiyo2008@gmail.com>
2024-12-02 13:04:40 +0100

    An Intro to JAJAVASCRIPT

--- 1-dataTypes.js
+++ 1-dataTypes.js
@@ -2,6 +2,50 @@
 console.log("Aliyah".length);
 console.log("Liya".length);
 
-
+// Uppercase
 //classwork 2
 console.log("aliyah".toUpperCase())
+
+// Lowercase
+console.log("All charactersto lowercase =>","I AM SUPER EXCITED TO START LEARNING JS".toLowerCase())
+
+//classwork 3
+console.log("check whether it contain a substring =>", "aliyah".includes("a"));
+
+// /index of a string characters/
+//string [index]
+console.log("first character","aliyah"[0]);
+console.log("second character", "aliyah"[1]);
+console.log("third character", "aliyah"[2]);
+console.log("fourth character", "aliyah"[3]);
+console.log("fifth character", "aliyah"[4]);
+console.log("sixth character", "aliyah"[5]);
+
+// chart
+// Returns the character at the specified index
+// Example: "aliyah".charAt(4)
+
+// * Slice(start, end): Extracts part of a string and returns it
+// classwork 4
+console.log("aliyah".slice(0, 4))
+
+//split(seperator): Splits a string into an array of substrings
+console.log("aliyah".split(""))
+
+//  Math methods
+// CLASSWORK 5
+console.log("Math random result btw 0 and 1 =>", Math.random());
+console.log("Math random result btw 0 and 5 =>", Math.random());
+console.log("Math random result btw 0 and 2 =>", Math.random() * 5);
+console.log("Math random result numbers from 1 to 10 =>", Math.random() * 10 + 1);
+
+console.log(
+  "Math random result numbers from 1 to 100 =>",
+  Math.random() * 100 + 1
+);
+console.log("RESULT OF RANDOM =>", Math.random() *100 + 1);
+
+
+
+
+
